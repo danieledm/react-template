55948325fdaf1dcbc4da7b7d2c28557614a1be03
Daniele DalleMule <ddallemule@ebay.com>
2017-09-08 17:54:55 +0200

    first commit

---
+++ src/main/g8/src/main.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+import Form from "react-jsonschema-form";
+
+import './css/style.css';
+
+
+
+const schema = {
+  title: "Todo",
+  type: "object",
+  required: ["title"],
+  properties: {
+    title: {type: "string", title: "Titolo", default: "A new task"},
+    done: {type: "boolean", title: "Done?", default: false},
+    attachment: {type: "array", items: { type: "string",  format: "data-url" }, title: "allegato" }
+  }
+};
+
+const formData = localStorage.dati ? JSON.parse(localStorage.dati) : {done:true, title: "ciccio bello"};
+
+const log = (type) => console.log.bind(console, type);
+
+const onSubmit = ({formData}) => localStorage.dati = JSON.stringify(formData);
+
+const uiSchema = {
+  attachment: {
+    "ui:options":  {
+      orderable: true,
+          addable: true,
+          removable: true
+
+    }
+  }
+}
+
+render((
+  <Form schema={schema}
+        formData={formData}
+        onChange={log("changed")}
+        uiSchema={uiSchema}
+        onSubmit={onSubmit}
+        onError={log("errors")} />
+), document.getElementById("app"));
+
+
+
---
+++ src/main/g8/webpack.config.js
@@ -0,0 +1,45 @@
+ var path = require('path');
+ var webpack = require('webpack');
+
+ module.exports = {
+     entry: './js/main.js',
+     output: {
+         path: path.resolve(__dirname, 'build'),
+         filename: 'main.bundle.js'
+     },
+     module: {
+         rules: [
+             {
+                 test: /\.js$$/,
+                 loader:  'babel-loader',
+                 query: {
+                     presets: ['es2015', 'react']
+                 }
+            },
+            { test: /\.jsx$$/, use: 'babel-loader', exclude: /node_modules/ },
+            {
+                test: /\.css$$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$$/,
+                use: [
+                    'file-loader'
+                ]
+           },
+           {
+                test: /\.(woff|woff2|eot|ttf|otf)$$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+     },
+     stats: {
+         colors: true
+     },
+     devtool: 'source-map'
+ };
